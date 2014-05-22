006d88e2de66ad2d9f5ad5c496e779caba1ed869
joris.calabrese <joris.calabrese@gmail.com>
2014-05-22 18:48:15 +0200

    Update my parser

--- Utility/Parser.php
+++ Utility/Parser.php
@@ -1,2 +1,27 @@
 <?php
 
+function test($child, $results)
+{
+	echo "Launch parser with ".$child->getName(). "\n";
+	if('file' == $child->getName()) {
+		$results[] = new FileMetric(
+			$child->metrics['name'],
+			$child->metrics['namespace'],
+			$child->metrics['methods'],
+			$child->metrics['coveredmethods'],
+			$child->metrics['statements'],
+			$child->metrics['coveredstatements']);
+		return $results;
+	} else {
+		if($child->count > 0) {
+			foreach(foreach($child->children as $newChild))
+			{
+				if('package' = $newChild->getName()) {
+					$result[] = $this->parser($newChild, $results);
+				}
+			}
+		} else {
+			return $results;
+		}
+	}
+}

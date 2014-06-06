e53b850f339f093a7abea8175e0001ecb1f6923e
joris.calabrese <joris.calabrese@gmail.com>
2014-06-06 18:36:03 +0200

    Add AngularJs as view

---
+++ web/js/project.js
@@ -0,0 +1,27 @@
+var parserApp = angular.module('parserApp', ['ui.bootstrap','ui.bootstrap.alert','ngRoute', 'ngResource']);
+
+parserApp.config(function ($httpProvider) {
+  $httpProvider.responseInterceptors.push('myHttpInterceptor');
+
+  var spinnerFunction = function spinnerFunction(data, headersGetter) {
+    $("#spinner").show();
+    return data;
+  };
+
+  $httpProvider.defaults.transformRequest.push(spinnerFunction);
+});
+
+parserApp.factory('myHttpInterceptor', function ($q, $window) {
+  return function (promise) {
+    return promise.then(function (response) {
+      $("#spinner").hide();
+      return response;
+    }, function (response) {
+      $("#spinner").hide();
+      return $q.reject(response);
+    });
+  };
+});
+
+var PARTIALS_PATH = '/web/partials/';
+
